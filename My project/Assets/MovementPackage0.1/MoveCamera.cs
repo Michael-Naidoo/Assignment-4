23b7a1eb5a2e2b852f38f728931702b07c579def
Michael-Naidoo <130663291+Michael-Naidoo@users.noreply.github.com>
2023-09-19 11:53:32 +0200

    Package Install

---
+++ My project/Assets/MovementPackage0.1/MoveCamera.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class MoveCamera : MonoBehaviour
+    {
+        public Transform cameraPosition;
+
+        private void Update()
+        {
+            transform.position = cameraPosition.transform.position;
+        }
+    }
+}
